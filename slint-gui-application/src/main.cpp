cea963e04701747498ef005c26d657314b523ae4
JM <jo3tech@outlook.com>
2026-01-19 21:38:04 +0000

    Registered method to open Github webpage.

--- slint-gui-application/src/main.cpp
+++ slint-gui-application/src/main.cpp
@@ -1,13 +1,25 @@
 #include "app_window.h"
 
+#include "utils.hpp"
+//#include <cstdint>
+#include <iostream>
+//#include <string>
+
 int main(int argc, char **argv)
 {
-    auto ui = AppWindow::create();
+  auto ui = AppWindow::create();
+
 /*
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
-    });
+  ui->on_request_increase_value([&]{
+      ui->set_counter(ui->get_counter() + 1);
+  });
 */
-    ui->run();
-    return 0;
+
+  ui->on_open_github([](slint::SharedString url_output) {
+    std::cout << "Open GitHub: " << url_output << std::endl;
+    utils::open_url(static_cast<std::string>(url_output));
+  });
+
+  ui->run();
+  return 0;
 }
